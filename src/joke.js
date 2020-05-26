249eecc85c8bac1268eb4a923bce969e85b47b6c
Kendra <kmodrunia@ucdavis.edu>
2020-05-26 13:13:05 -0700

    made custom useFetch

--- src/joke.js
+++ src/joke.js
@@ -1,24 +1,31 @@
 import React, {useEffect, useState} from 'react';
 
+const useFetch = (url, initialValue) =>{
+    const [result, setResult] = useState(initialValue);
+
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(json => setResult(json))
+    }, []);
+
+    return result
+}
+
 function Joke() {
-
-    const [joke, setJoke] = useState({});
-
-    useEffect(()=> {
-        fetch(`https://official-joke-api.appspot.com/jokes/random`)
-        // fetch(`http://localhost:3005/jokes/random`)
-            .then( response => response.json())
-            .then(json => {
-                console.log(json)
-                setJoke(json)
-            })
-    },[]);
+    const { setup, punchline } = useFetch(`https://official-joke-api.appspot.com/jokes/random`,{})
+    // useEffect(()=> {
+    //     fetch(`https://official-joke-api.appspot.com/jokes/random`)
+    //     // fetch(`http://localhost:3005/jokes/random`)
+    //         .then( response => response.json())
+    //         .then(json => {
+    //             console.log(json)
+    //             setJoke(json)
+    //         })
+    // },[]);
     //acts as component did mount
     //use effect runs its call back at every render (componentdidupdate);
     //the empty array will avoid the infinite loop of componentdidupdate
-
-    const {setup, punchline} = joke;
-
     return(
         <div>
             <h3>Joke</h3>
