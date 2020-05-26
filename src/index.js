88734c749c63b992f68e680d73e2ab0b3e0ee865
Kendra <kmodrunia@ucdavis.edu>
2020-05-26 14:00:02 -0700

    demo arrow vs function of this

--- src/index.js
+++ src/index.js
@@ -10,3 +10,23 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+// function createObject(){
+//   console.log('outtermost this', this);
+//   return {
+//     arrowFunction: () =>{
+//       console.log('arrowFunction this', this);
+//     },
+//     functionKeywordFunction: function(){
+//       console.log('functionKeyword this', this);
+//     }
+//   }
+// }
+
+// const obj = createObject();
+// console.log('obj',obj);
+
+// obj.arrowFunction(); //undefined, use when you don't depend on this
+// obj.functionKeywordFunction(); //set to the obj created with
+
