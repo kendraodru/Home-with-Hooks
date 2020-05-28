85e31f21bc514e1e0d8bab82b6acd90c5c7fee1f
Kendra <kmodrunia@ucdavis.edu>
2020-05-28 13:23:19 -0700

    cleanedup

--- src/matrix.js
+++ src/matrix.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import MATRIX_FRAMES from './data/matrix';
 import {useDynamicTransition} from './hooks';
 
